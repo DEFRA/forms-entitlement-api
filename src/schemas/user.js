46d0073f9937debf805c6c93db33673d8cfeb48f
David Stone <davidjamesstone@gmail.com>
2026-02-19 14:13:45 +0000

    Update endpoint schema to support new roles

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -8,12 +8,12 @@ export const userIdSchema = Joi.object().keys({
 export const createUserSchema = Joi.object().keys({
   email: Joi.string().email().required(),
   roles: Joi.array()
-    .items(Joi.string().valid(Roles.Admin, Roles.FormCreator))
+    .items(Joi.string().valid(...Object.values(Roles)))
     .required()
 })
 
 export const updateUserSchema = Joi.object().keys({
   roles: Joi.array()
-    .items(Joi.string().valid(Roles.Admin, Roles.FormCreator))
+    .items(Joi.string().valid(...Object.values(Roles)))
     .required()
 })
